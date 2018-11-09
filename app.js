fa71cca35c635e3f5d7b210f357c726dda2566cc
Zaki Wasik <zaki@webconsu.lt>
2018-11-09 20:45:50 +0100

    Add logout button and conditionally render nav items

--- app.js
+++ app.js
@@ -55,6 +55,11 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
+app.use(function(req, res, next) {
+  res.locals.isAuthenticated = req.isAuthenticated();
+  next();
+})
+
 app.use('/', index);
 app.use('/users', users);
 
